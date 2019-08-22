9fa5e38ca0aaadb8106aae792331aa4d67de48ca
Hans Cornelis <hacornelis@gmail.com>
2019-08-22 07:46:01 +0200

    Beautifying changes

--- init.sh
+++ init.sh
@@ -28,7 +28,6 @@ if [[ ${DOMAIN_EXTENSION} =~ [.] ]]; then
 fi
 sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/mods-available/ldap
 
-
 # Handle the certs
 cp /certs/ldap-client.key /etc/freeradius/certs/ldap-client.key
 cp /certs/ldap-client.crt /etc/freeradius/certs/ldap-client.crt
@@ -41,9 +40,9 @@ FILES_640=( "ca.key" "server.key" "server.p12" "server.pem" "ldap-client.crt" "l
 for i in "${FILES_640[@]}"
 do
 	if [ -f "/certs/$i" ]; then
-    cp /certs/$i /etc/raddb/certs/$i
-    chmod 640 /etc/raddb/certs/$i
-fi
+	    cp /certs/$i /etc/raddb/certs/$i
+	    chmod 640 /etc/raddb/certs/$i
+	fi
 done
 
 # Now all files that need a 644 permission set
@@ -51,9 +50,9 @@ FILES_644=( "ca.pem" "server.crt" "server.csr" "dh" )
 for i in "${FILES_644[@]}"
 do
 	if [ -f "/certs/$i" ]; then
-    cp /certs/$i /etc/raddb/certs/$i
-    chmod 644 /etc/raddb/certs/$i
-fi
+	    cp /certs/$i /etc/raddb/certs/$i
+	    chmod 644 /etc/raddb/certs/$i
+	fi
 done
 
 
